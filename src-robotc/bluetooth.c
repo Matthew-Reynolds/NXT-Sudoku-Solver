64125d55dda1efbd37d36a275394b1072e413ff3
Matthew Reynolds <rey.mtr@gmail.com>
2017-11-17 00:20:41 -0500

    Wrote pseudo-code for bluetooth

--- src-robotc/bluetooth.c
+++ src-robotc/bluetooth.c
@@ -2,17 +2,44 @@
 
 
 BT_Status establishConnection(long timeout){
-	// Code me!!
+
+	// see http://www.robotc.net/wikiarchive/NXT_Functions_Bluetooth
+
+	// The flow is probably:
+	// 1. turn on BT
+	// 2. Set friendly name?? eg. Solver or Main. Probably only need to do this once (manually) since it remembers?
+	// 3. set BT to visible
+	// 4. btSearch?? This will add all BT devices it finds. Makes it easy to connect to them? But we should need to do this since it will remember, so we can find the other brick manually one time probably?
+	// 5. btConnect(port, name) Connect to the brick nammed 'name', on the port 'port' (1,2,3).
+	// Repeat until we successfully connect or we hit the timeout
+
+	// We might have to bBTSkipPswdPrompt = true to skip entering a password when we connect???
+
+	// Attempts to connect a BT device with a specified sFriendlyName on port nPort. nPort should be either 1, 2, or 3.
+	// void btConnect(int nPort, string sFriendlyName)
 }
 
 
 BT_Status sendPuzzle(const Sudoku & sudoku, long timeout){
-	// Code me!!
+
+	// Use this to send the data.
+	// We will need to send the puzzle in 9 messages (Each row) due to size limitations.
+	// Each message which will get stored in a queue on the specified mailbox
+
+	// Use the appropriate mailbox defined in bluetooth.h
+	// Message should be an array of ubyte with size const messageLength=9
+	// cCmdMessageWriteToBluetooth(mailbox, message, messageLength)
 }
 
 
 BT_Status receivePuzzle(Sudoku & sudoku, bool & isSolved, long timeout){
-	// Code me!!
+
+	// Use the function cCmdMessageGetSize(mailbox) to determine whether any messages have been received.
+	// A positive return value indicates that a message was received and is the length of the message.
+
+	// Same as send but in reverse. Read all 9 rows.
+	// We will also need to read the solved/not solved message from the other mailbox (See bluetooth.h)
+	// cCmdMessageRead(mailbox, message, messageLength)
 }
 
 
--- src-robotc/bluetooth.h
+++ src-robotc/bluetooth.h
@@ -7,6 +7,16 @@ enum BT_Status {
 	BT_ERROR
 };
 
+// Mailbox 1: Main->Solver [2]	{??} // Not needed??
+// Mailbox 2: Main->Solver [9]	{puzzle row}
+// Mailbox 3: Solver->Main [2]	{flag isSolved}
+// Mailbox 4: Solver->Main [9] {puzzle row}
+const TMailboxIDs bt_sendData = mailbox1;
+const TMailboxIDs bt_sendPuzzle = mailbox2;
+const TMailboxIDs bt_getData = mailbox3;
+const TMailboxIDs bt_getPuzzle = mailbox4;
+
+
 // IMPLEMENT THESE 3
 BT_Status establishConnection(long timeout);
 BT_Status sendPuzzle(const Sudoku & sudoku, long timeout);
