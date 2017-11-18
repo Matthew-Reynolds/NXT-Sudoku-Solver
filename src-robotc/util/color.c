9719bb8a2a1c78d50754eaa752f27e7874f59f28
Matthew Reynolds <rey.mtr@gmail.com>
2017-11-18 14:51:05 -0500

    Updated color.c and color.h
    Added new thresholds for the florescent/sunlight of the WatIMake lab. These thresholds are similar, but not exactly the same, as the light was much more white/less yellow than in my room.
    
    Also changed the color thresholding to capture the average threshold over multiple readings rather than just one single reading.

--- src-robotc/util/color.c
+++ src-robotc/util/color.c
@@ -1,5 +1,32 @@
 #include "color.h"
 
+
+void getAvgColor(short * rgb, int readings, long interval){
+	short history[3] = {0,0,0};
+	long redTotal = 0;
+	long greenTotal = 0;
+	long blueTotal = 0;
+
+	for(int curReading = 0; curReading < readings; curReading ++){
+
+		// Read the R,G,B,A values into an array
+		getColorSensorData(colorSensor, colorRaw, &history[0]);
+
+		redTotal += history[0];
+		greenTotal += history[1];
+		blueTotal += history[2];
+	}
+
+	rgb[0] = redTotal/readings;
+	rgb[1] = greenTotal/readings;
+	rgb[2] = blueTotal/readings;
+}
+
+
+bool isInRGBPercent(const short * rgb, const short * percents, short thresh){
+	return isInRGBPercent(rgb, percents[0], percents[1], percents[2], thresh);
+}
+
 bool isInRGBPercent(const short * rgb, short red, short green, short blue, short thresh){
 
 	// Handle divide by 0
@@ -44,13 +71,13 @@ bool isInRGBRange(const short * rgb, short minR, short minG, short minB, short m
 	// return false.
 	for(int cur = 0; cur < 3; cur++)
 		if(min[cur] > rgb[cur] || rgb[cur] > max[cur]){
-			isInRange = false;
+		isInRange = false;
 
 #ifdef _DEBUG
-			displayTextLine(5, "%d < %d < %d", min[cur], rgb[cur], max[cur]);
+		displayTextLine(5, "%d < %d < %d", min[cur], rgb[cur], max[cur]);
 #endif
 
-		}
+	}
 
 	// Otherwise, return true
 	return isInRange;
@@ -59,8 +86,9 @@ bool isInRGBRange(const short * rgb, short minR, short minG, short minB, short m
 int getCellValue(){
 
 	// Read the R,G,B,A values into an array
-	short rgb[4];
-	getColorSensorData(colorSensor, colorRaw,    &rgb[0]);
+	short rgb[4] = {0,0,0,0};
+	//getColorSensorData(colorSensor, colorRaw,    &rgb[0]);
+	getAvgColor(rgb);
 
 #ifdef _DEBUG
 	displayTextLine(0, "%3d %3d %3d", rgb[0], rgb[1], rgb[2]);
@@ -69,39 +97,39 @@ int getCellValue(){
 	int cellNum = 0;
 
 	// Red
-	if(isInRGBPercent(rgb, 58, 22, 21))
+	if(isInRGBPercent(rgb, RGB_RED_THRESH))
 		cellNum = 1;
 
 	// Green
-	else if(isInRGBPercent(rgb, 29, 48, 23))
+	else if(isInRGBPercent(rgb, RGB_GREEN_THRESH))
 		cellNum = 2;
 
 	// Dark Blue
-	else if(isInRGBPercent(rgb, 24, 33, 43, 3))
+	else if(isInRGBPercent(rgb, RGB_DBLUE_THRESH, 3))
 		cellNum = 3;
 
 	// Yellow
-	else if(isInRGBPercent(rgb, 44, 41, 15))
+	else if(isInRGBPercent(rgb, RGB_YELLOW_THRESH))
 		cellNum = 4;
 
 	// Light Blue
-	else if(isInRGBPercent(rgb, 22, 37, 40, 3))
+	else if(isInRGBPercent(rgb, RGB_LBLUE_THRESH, 3))
 		cellNum = 5;
 
 	// Pink
-	else if(isInRGBPercent(rgb, 49, 20, 31))
+	else if(isInRGBPercent(rgb, RGB_PINK_THRESH))
 		cellNum = 6;
 
 	// Purple
-	else if(isInRGBPercent(rgb, 29, 30, 41, 3))
+	else if(isInRGBPercent(rgb, RGB_PURPLE_THRESH, 3))
 		cellNum = 7;
 
 	// Brown
-	if(isInRGBPercent(rgb, 47, 30, 27))
+	else if(isInRGBPercent(rgb, RGB_BROWN_THRESH))
 		cellNum = 8;
 
 	// Orange
-	else if(isInRGBPercent(rgb, 56, 28, 16))
+	else if(isInRGBPercent(rgb, RGB_ORANGE_THRESH))
 		cellNum = 9;
 
 	return cellNum;
--- src-robotc/util/color.h
+++ src-robotc/util/color.h
@@ -1,9 +1,64 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-
+void getAvgColor(short * rgb, int readings = 5, long interval = 10);
+bool isInRGBPercent(const short * rgb, const short * percents, short thresh = 5);
 bool isInRGBPercent(const short * rgb, short red, short green, short blue, short thresh = 5);
 bool isInRGBRange(const short * rgb, short minR, short minG, short minB, short maxR, short maxG, short maxB);
 int getCellValue();
+
+
+
+// LOAD THE CONSTANTS FOR THE RGB THRESHOLDS
+//#define LOCATION_SJU
+#define LOCATION_WATIMAKE
+//#define LOCATION_WEEF
+
+#ifdef LOCATION_SJU
+const short RGB_RED_THRESH[3] = 		{58, 22, 21};
+const short RGB_GREEN_THRESH[3] =		{29, 48, 23};
+const short RGB_DBLUE_THRESH[3] =		{24, 33, 43};
+const short RGB_YELLOW_THRESH[3] =	{44, 41, 15};
+const short RGB_LBLUE_THRESH[3] =		{22, 37, 40};
+const short RGB_PINK_THRESH[3] =		{49, 20, 31};
+const short RGB_PURPLE_THRESH[3] =	{29, 30, 41};
+const short RGB_BROWN_THRESH[3] =		{47, 30, 23};
+const short RGB_ORANGE_THRESH[3] =	{56, 28, 16};
+
+#elif defined(LOCATION_WATIMAKE)
+const short RGB_RED_THRESH[3] = 		{64, 19, 17};
+const short RGB_GREEN_THRESH[3] =		{26, 52, 22};
+const short RGB_DBLUE_THRESH[3] =		{22, 35, 43};
+const short RGB_YELLOW_THRESH[3] =	{46, 43, 10};
+const short RGB_LBLUE_THRESH[3] =		{18, 40, 42};
+const short RGB_PINK_THRESH[3] =		{56, 17, 27};
+const short RGB_PURPLE_THRESH[3] =	{26, 29, 44};
+const short RGB_BROWN_THRESH[3] =		{47, 32, 20};
+const short RGB_ORANGE_THRESH[3] =	{63, 25, 13};
+
+
+#elif defined(LOCATION_WEEF)
+const short RGB_RED_THRESH[3] = 		{58, 22, 21};
+const short RGB_GREEN_THRESH[3] =		{29, 48, 23};
+const short RGB_DBLUE_THRESH[3] =		{24, 33, 43};
+const short RGB_YELLOW_THRESH[3] =	{44, 41, 15};
+const short RGB_LBLUE_THRESH[3] =		{22, 37, 40};
+const short RGB_PINK_THRESH[3] =		{49, 20, 31};
+const short RGB_PURPLE_THRESH[3] =	{29, 30, 41};
+const short RGB_BROWN_THRESH[3] =		{47, 30, 23};
+const short RGB_ORANGE_THRESH[3] =	{56, 28, 16};
+
+#else
+const short RGB_RED_THRESH[3] = 		{0, 0, 0};
+const short RGB_GREEN_THRESH[3] =		{0, 0, 0};
+const short RGB_DBLUE_THRESH[3] =		{0, 0, 0};
+const short RGB_YELLOW_THRESH[3] =	{0, 0, 0};
+const short RGB_LBLUE_THRESH[3] =		{0, 0, 0};
+const short RGB_PINK_THRESH[3] =		{0, 0, 0};
+const short RGB_PURPLE_THRESH[3] =	{0, 0, 0};
+const short RGB_BROWN_THRESH[3] =		{0, 0, 0};
+const short RGB_ORANGE_THRESH[3] =	{0, 0, 0};
+
+#endif
 
 #endif
