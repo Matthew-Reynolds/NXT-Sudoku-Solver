0adc27ab0909ba5c4818190244382d3646cbfcc9
Matthew Reynolds <rey.mtr@gmail.com>
2017-11-22 12:22:22 -0500

    Cleaned up the main program flow

--- src-robotc/main.c
+++ src-robotc/main.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+Sudoku board1 =
+{2, 0, 0, 0, 4, 0, 0, 3, 8,
+	0, 3, 0, 0, 0, 2, 0, 1, 9,
+	0, 9, 8, 3, 0, 1, 6, 2, 0,
+	0, 0, 3, 2, 0, 0, 1, 0, 0,
+	0, 5, 0, 0, 0, 0, 0, 6, 0,
+	0, 0, 6, 0, 0, 7, 2, 0, 0,
+	0, 7, 5, 4, 0, 3, 9, 8, 0,
+	1, 2, 0, 7, 0, 0, 0, 5, 0,
+	3, 8, 0, 0, 6, 0, 0, 0, 2};
+
 RobotState state = STATE_DISABLED;
 
 task homeThread(){
@@ -29,6 +40,16 @@ task pidUpdateThread(){
 	}
 }
 
+void displayError(BT_Status error, int timeout = 3){
+	displayCenteredTextLine(2, "Error:");
+	displayCenteredTextLine(3, getStatusMessage(error));
+	playSound(soundException);
+	for(;timeout > 0; timeout--){
+		displayCenteredTextLine(4, "Retrying in %d...", timeout);
+		wait1Msec(1000);
+	}
+}
+
 task main()
 {
 	// Configure the sensors
@@ -46,32 +67,38 @@ task main()
 	nVolume = 4;
 
 	// Establish a Bluetooth connection
-	/*
-	displayCenteredTextLine(2, "Establishing BT");
-	displayCenteredTextLine(4, "Connection...");
+
+	displayCenteredTextLine(0, "Establishing BT");
+	displayCenteredTextLine(1, "Connection...");
 	setupBluetooth();
 	BT_Status status = establishConnection(-1);
 	displayCenteredTextLine(6, "%s", getStatusMessage(status));
 	if(status != BT_SUCCESS){
-	playSound(soundException);
-	wait1Msec(5000);
-	return;
+		displayError(status, 0);
+		wait1Msec(5000);
+		return;
 	}
 	else {
-	playSound(soundFastUpwardTones);
-	wait1Msec(5000);
-	}*/
+		playSound(soundFastUpwardTones);
+
+		eraseDisplay();
+		displayCenteredTextLine(0, "Connected to");
+		displayCenteredTextLine(1, "Solver!");
+		wait1Msec(2000);
+	}
 
 	// Initialize the PI controllers
-	initializeController(controllers[0], 0.8, 0.00005, MOTOR_ENCODER, motorA, motorA);
-	setInputRange(controllers[0], -32767, 32767, 360.0/(11) * (40/8));	//11cm per rotation, 8:40 reduction
+	initializeController(controllers[0], 0.8, 0.00005, MOTOR_ENCODER, yAxisMotor, yAxisMotor);
+	setInputRange(controllers[0], -32767, 32767, 360.0/(12.2) * (40/8));	//11cm per rotation, 8:40 reduction
 	setOutputRange(controllers[0], -100, 100);
-	setTolerance(controllers[0], 2);
+	setTolerance(controllers[0], 3); // 3 degrees = 0.02cm
 
-	initializeController(controllers[1], 0.9, 0.0001, MOTOR_ENCODER, motorB, motorB);
-	setInputRange(controllers[1], -32767, 32767, 360.0/2.555); // 2.555cm per rev
+//	initializeController(controllers[1], 0.9, 0.0005, MOTOR_ENCODER, motorB, motorB);
+	initializeController(controllers[1], 0.7, 0.001, MOTOR_ENCODER, xAxisMotor, xAxisMotor);
+	setInputRange(controllers[1], -32767, 32767, 360.0/2.6); // 2.555cm per rev
 	setOutputRange(controllers[1], -100, 100);
-	setTolerance(controllers[1], 1);
+	setTolerance(controllers[1], 3); // 3 degrees = 0.02cm
+	//Old: 1
 
 	// Set the motors to coast mode
 	bFloatDuringInactiveMotorPWM = true;
@@ -83,66 +110,61 @@ task main()
 
 	// =*=*=*=*=*=*=*=*= MAIN LOOP =*=*=*=*=*=*=*=*=
 	state = STATE_RUNNING;
+	setSetpoint(controllers[0], 0);
+	setSetpoint(controllers[1], 0);
 	startTask(pidUpdateThread);
 	controllers[0].isEnabled = true;
 	controllers[1].isEnabled = true;
-
 
 	Sudoku solved;
 	Sudoku puzzle;
 	bool puzzleIsSolved = false;
 
-	// Print 1-9, then 9-1
-	for(int col = 0; col < 9; col++){
-		moveToCell(0,col,true);
-		plotNumber(col+1);
-		wait1Msec(500);
-	}
-	for(int col = 0; col < 9; col++){
-		moveToCell(1,col,true);
-		plotNumber(8-col+1);
-		wait1Msec(500);
-	}
-
-	return;
-
-	findFirstCell();
 	readPuzzle(puzzle);
+	//printPuzzle(puzzle);
+	//wait1Msec(5000);
 
+	memcpy(puzzle, board1, sizeof(board1[0][0])*81);
+
+	eraseDisplay();
+	displayCenteredTextLine(0, "Sending puzzle:");
 	// Send the puzzle over bluetooth
-	BT_Status status = sendPuzzle(puzzle, false);
+	status = sendPuzzle(puzzle, false);
 	while(status != BT_SUCCESS){
 
 		// Everytime there is an error, output the error message and make a sad sound
-		displayBigTextLine(2, "Send file: %s",  getStatusMessage(status));
-		playSound(soundException);
+		displayError(status);
 		status = sendPuzzle(puzzle, false);
-		eraseDisplay();
 	}
 
+	playSound(soundFastUpwardTones);
+	eraseDisplay();
+	displayCenteredTextLine(0, "Sent puzzle");
+	displayCenteredTextLine(1, "Waiting for soln");
+	wait1Msec(1000);
 
 	// Recieve the puzzle from bluetooth
-	status = receivePuzzle(puzzle, puzzleIsSolved, -1);
+	status = receivePuzzle(solved, puzzleIsSolved, -1);
 	while(status != BT_SUCCESS){
 
 		// Everytime there is an error, output the error message and make a sad sound
-		displayBigTextLine(2, "Send file: %s",  getStatusMessage(status));
-		playSound(soundException);
+		displayError(status);
 		status = receivePuzzle(solved, puzzleIsSolved, -1);
-		eraseDisplay();
 	}
 
+	playSound(soundFastUpwardTones);
+	eraseDisplay();
+	displayCenteredTextLine(0, "Recieved soln");
+	wait1Msec(1000);
 
 	// If the puzzle was solved, print it out.
-	if(puzzleIsSolved){
-
-		//TODO: Offset the head to center the pen in the cell rather than the color sensor
+	if(puzzleIsSolved)
 		printPuzzle(puzzle, solved);
-	}
 
 	// Otherwise, make a sad noise and print out unsolveable
 	else {
-		displayBigTextLine(2, "Unable to solve puzzle :(");
+		displayCenteredTextLine(0, "Unable to solve");
+		displayCenteredTextLine(1, "puzzle :(");
 		playSound(soundException);
 	}
 
